44a527f6e9ada483a49ed1efa8af5802a3130544
scrigroup2013 <scrigroup2013@gmail.com>
2019-08-10 22:32:09 -0500

    create models

---
+++ API/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PartialTitle { get; set; }
+        public string ReleaseYear { get; set; }
+        public List<string> Genres { get; set; }
+    }
+}
